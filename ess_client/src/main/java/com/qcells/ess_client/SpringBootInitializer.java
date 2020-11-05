4195c04575b06359770efb5ce05c5238dc278f4a
terryaaaa <terryaaaa@naver.com>
2020-11-05 09:09:21 +0900

    change to develop

--- ess_client/src/main/java/com/qcells/ess_client/SpringBootInitializer.java
+++ ess_client/src/main/java/com/qcells/ess_client/SpringBootInitializer.java
@@ -12,7 +12,7 @@ public class SpringBootInitializer {
 	public static void main(String[] args) {
         String profile = System.getProperty("spring.profiles.active");
         if(profile == null) {
-            System.setProperty("spring.profiles.active", "production");
+            System.setProperty("spring.profiles.active", "develop");
         }
 		SpringApplication.run(SpringBootInitializer.class, args);
 	}
